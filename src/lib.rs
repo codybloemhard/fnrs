9e0a76c947e8d1f39bc82dbb492479dc76012a9b
Cody Bloemhard <cblmhrd@live.nl>
2020-02-21 23:56:46 +0100

    carry over map from music project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+pub fn map<T,F>(inp: &[T], f: &F) -> Vec<T>
+    where
+        F: Fn(&T) -> T,
+{
+    let mut res = Vec::new();
+    for x in inp{
+        res.push(f(x));
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+    #[test]
+    fn test_map(){
+        assert_eq!(map(&vec![2,3,4], &|x| x + 1), vec![3,4,5]);
     }
 }
